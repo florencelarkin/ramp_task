99860d6fbd91c74e724b225d8f860f4fff0552d4
florencelarkin <67938867+florencelarkin@users.noreply.github.com>
2020-10-12 23:51:53 -0500

    updated slider method and appearance

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_xlider/flutter_xlider.dart';
-import 'package:control_pad/control_pad.dart';
-
-
+import 'dart:math';
 
 class MainPage extends StatefulWidget {
   @override
@@ -19,13 +16,8 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin{
   }
 
   AnimationController _controller;
-
-
   Stopwatch stopwatch = new Stopwatch()..start();
   int time = 0;
-  double instVelocity = 0;
-  double _lowerValue = 50.0;
-  double _upperValue = 50.0;
   double width = 100.0;
   double carStartPos = -5.0;
   double joyStickPos = 0.0;
@@ -33,9 +25,9 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin{
   double dy = 0.0;
 
   double getPos (joyStick, currentPos) {
-      dy = ((-.35*currentPos)+(-joyStick*150.0))*0.033;
+      dy = ((-.35*currentPos)+(-joyStick*50.0))*0.033;
       getCurrentPos = dy + currentPos;
-      dy = ((-.35*getCurrentPos)+(-joyStick*150.0))*0.033;
+      dy = ((-.35*getCurrentPos)+(-joyStick*50.0))*0.033;
     return dy;
   }
 
@@ -57,7 +49,7 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin{
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             SizedBox(
-              height: 200.0,
+              height: 100.0,
             ),
             AnimatedBuilder(
               animation: _controller,
@@ -69,82 +61,40 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin{
               ),
               builder: (BuildContext context, Widget child) {
                 return Transform.translate(
-                  offset: Offset(0.0, joyStickPos),
+                  offset: Offset(0.0, getCurrentPos),
                   child: child,
                 );
               },
             ),
-
             Container(
-              height: 100.0,
-              child: SliderTheme(
-                data: SliderTheme.of(context).copyWith(
-                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                  overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
-                  thumbColor: Color(0xFFEB1555),
-                  activeTrackColor: Colors.white,
-                  overlayColor: Color(0x29EB1555),
-                  inactiveTrackColor: Color(0xFF8D8E98),
-                ),
-                child: Slider(
-                  value: joyStickPos,
-                  min: -100.0,
-                  max: 100.0,
-                  onChanged: (double newValue) {
-                    setState(() {
-                      joyStickPos = newValue;
-                    });
-                  },
-                ),
-              ),
-              /*child: FlutterSlider(
-                values: [_lowerValue, _upperValue],
-                min: -100,
-                max: 100,
-                axis: Axis.vertical,
-                selectByTap: false,
-                *//*tooltip: FlutterSliderTooltip(
-                  disabled: true,
-                ),*//*
-                handlerAnimation: FlutterSliderHandlerAnimation(
-                    curve: Curves.elasticOut,
-                    duration: Duration(milliseconds: 500),
-                    scale: 1
-                ),
-                handler: FlutterSliderHandler(
-                  decoration: BoxDecoration(),
-                  child: Material(
-                    type: MaterialType.canvas,
-                    color: Colors.black.withOpacity(0),
-                    child: Container(
-                      color: Colors.black.withOpacity(0),
-                        child: Icon(Icons.radio_button_unchecked, size: 50),
+              height: 50.0,
+                child: Transform.scale(
+                  scale: 0.25,
+                  child: Transform.rotate(
+                    angle: pi/2,
+                    child: Slider(
+                      inactiveColor: Colors.black,
+                      activeColor: Colors.black,
+                      value: joyStickPos,
+                      min: -100.0,
+                      max: 100.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          joyStickPos = newValue / 100;
+                          print(newValue);
+                          dy = ((-.35*getCurrentPos)+(-joyStickPos*50.0))*0.033;
+                          getCurrentPos = dy + getCurrentPos;
+                        });
+                      },
+                      onChangeEnd: (double newValue) {
+                        setState(() {
+                          newValue = 0;
+                        });
+                      },
                     ),
                   ),
                 ),
-                trackBar: FlutterSliderTrackBar(
-
-                  inactiveTrackBar: BoxDecoration(
-                    color: Colors.grey
-                  ),
-                  activeTrackBar: BoxDecoration(
-                      color: Colors.grey
-                  ),
-                ),
-                onDragging: (handlerIndex, lowerValue, upperValue) {
-                  _lowerValue = lowerValue;
-                  _upperValue = upperValue;
-                  setState(() {
-                    joyStickPos = _upperValue;
-
-                    time = stopwatch.elapsedMilliseconds;
-                    //getAcceleration(instVelocity, time);
-                  });
-
-                },
-              ),*/
-            ),
-
+              ),
           ],
     ),
       ),
